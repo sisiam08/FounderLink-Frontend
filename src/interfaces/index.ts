065da19202af8dd5c0d16c5c90beff797e507438
sisiam08 <mohammadsisiam08@gmail.com>
2026-09-10 19:03:31 +0600

    add all requirements api's

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -2,3 +2,5 @@ export type * from "./auth.interface";
 export type * from "./user.interface";
 export type * from "./session.interface";
 export type * from "./profile.interface";
+export type * from "./startup.interface";
+export type * from "./application.interface";
