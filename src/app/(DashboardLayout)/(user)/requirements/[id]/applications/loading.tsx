35fbae52af44d6404c24ba834d3b7c3b685a535a
Taneem69 <tanvirtaneem32@gmail.com>
2026-09-12 01:17:48 +0600

    created application loading component

---
+++ src/app/(DashboardLayout)/(user)/requirements/[id]/applications/loading.tsx
@@ -0,0 +1,6 @@
+import { SkeletonRows } from "@/components/shared/skeletons";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function ApplicantsLoading(){
+    return <SkeletonRows/>
+}
