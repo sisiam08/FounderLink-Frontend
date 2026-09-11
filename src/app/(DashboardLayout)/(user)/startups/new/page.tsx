b2e9a857a29e0509a5256acb368614f445335748
PulockKundu <pulock7643@gmail.com>
2026-09-11 19:46:36 +0600

    card added in new startup page

--- src/app/(DashboardLayout)/(user)/startups/new/page.tsx
+++ src/app/(DashboardLayout)/(user)/startups/new/page.tsx
@@ -1,6 +1,5 @@
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
-
 import StartupForm from "../../_component/startup/startup-form";
 import {
   Card,
@@ -13,15 +12,15 @@ import {
 export default async function NewStartupPage() {
   return (
     <div className="max-w-2xl space-y-6">
-      <Link
-        href="/startups/mine"
-        className="-ml-2 inline-flex items-center gap-1 rounded-md px-2.5 py-2 text-sm font-medium text-muted-foreground transition-colors hover:bg-muted hover:text-foreground"
+    <Link
+       href="/startups/mine"
+      className="-ml-2 inline-flex items-center gap-1 rounded-md px-2.5 py-2 text-sm font-medium text-muted-foreground transition-colors hover:bg-muted hover:text-foreground"
       >
-        <ArrowLeft className="size-4" />
-        Back
+      <ArrowLeft className="size-4" />
+          Back
       </Link>
 
-      <Card>
+     <Card>
         <CardHeader>
           <CardTitle className="text-2xl">New Startup Idea</CardTitle>
           <CardDescription>
