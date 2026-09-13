971e9b0723159a8997789d12c5d38d5c6bfcbc3a
Taneem69 <tanvirtaneem32@gmail.com>
2026-09-14 00:58:01 +0600

    added admin requirements page

---
+++ src/app/(DashboardLayout)/(admin)/admin/requirements/page.tsx
@@ -0,0 +1,22 @@
+import type { AdminRequirementsPage } from "@/interfaces";
+import { RequirementsClient } from "../../_component/requirement/requirements-client";
+import { getAdminRequirements } from "@/services/admin.service";
+
+export const dynamic = "force-dynamic";
+
+export default async function AdminRequirementsPage() {
+  let data: AdminRequirementsPage = {
+    requirements: [],
+    total: 0,
+    page: 1,
+    limit: 20,
+  };
+
+  return (
+    <RequirementsClient
+      initialRequirements={data.requirements}
+      initialTotal={data.total}
+      initialLimit={data.limit}
+    />
+  );
+}
